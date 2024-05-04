d0c70611bc80b4b5fb60b9f1a397f7390a72fce8
camila santibañez <cspavez5@gmail.com>
2024-05-04 18:38:02 -0400

    info en app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import Formulario from './componentes/Formulario';
-import Alert from './componentes/Alert';
-import Buscador from './componentes/Buscador';
+import Listado from './componentes/Listado';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
