1ddd1d107c807f19436f29f5107b74ced0062eab
camila santibañez <cspavez5@gmail.com>
2024-05-04 18:17:38 -0400

    se edita lista

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,58 @@
-import { useState } from "react"
-import { BaseColaboradores } from "./assets/BaseColaboradores"
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App () { 
-  const [personas, setPersonas] = useState(BaseColaboradores)
-  console.log(personas)
+const CollaboratorsList = () => {
+  const collaborators = [
+    {
+      name: 'Juan Soto',
+      email: 'juans@colaborador.com',
+      age: 23,
+      role: 'Desarrollador FrontEnd',
+      phone: '99887766',
+    },
+    {
+      name: 'Lucas Pailamilla',
+      email: 'lucasp@colaborador.com',
+      age: 31,
+      role: 'Desarrollador Backend',
+      phone: '88779955',
+    },
+    {
+      name: 'Diego Riquelme',
+      email: 'diegor@colaborador.com',
+      age: 28,
+      role: 'Ingeniero DevOps',
+      phone: '99226644',
+    },
+  ];
+
   return (
+    <div className="container">
+      <h1 style={{fontSize:"25px", textAlign:"center"}}>Lista de Colaboradores</h1>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Correo</th>
+            <th>Edad</th>
+            <th>Cargo</th>
+            <th>Teléfono</th>
+          </tr>
+        </thead>
+        <tbody>
+          {collaborators.map((collaborator) => (
+            <tr key={collaborator.email}>
+              <td>{collaborator.name}</td>
+              <td>{collaborator.email}</td>
+              <td>{collaborator.age}</td>
+              <td>{collaborator.role}</td>
+              <td>{collaborator.phone}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-    <>
-      <h1> Hola mundo! </h1>
-      {personas.map (dato => 
-      <article>
-        <h3>{dato.nombre}</h3>
-        <h3>{dato.correo}</h3>
-        <h3>{dato.edad}</h3>
-        <h3>{dato.cargo}</h3>
-        <h3>{dato.telefono}</h3>
-      
-      
-      </article>)}
-    </>
-  )
-
-}
-  
-
-export default App
+export default CollaboratorsList;
